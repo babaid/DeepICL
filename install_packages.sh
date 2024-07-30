d78541dc334d5514e9f4ba0b76dd2280629270f3
Dezsö Babai <de.babai2000@gmail.com>
2024-07-30 14:12:49 +0200

    Update install_packages.sh
    Updated accordingly to the colab notebook and use mamba instead of conda

--- install_packages.sh
+++ install_packages.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # 1. Create conda environment
-conda create -n DeepICL python=3.7.13 -y
-conda activate DeepICL
+mamba create -n DeepICL python=3.7.13 -y
+mamba activate DeepICL
 
 # 2. install packages dependent on mkl
-conda install scipy=1.6.2 numpy=1.21.2 pandas=1.3.4 scikit-learn=1.0.2 seaborn=0.11.0 -y
+mamba install scipy=1.7.3 numpy=1.21.1 pandas=1.3.4 scikit-learn=1.0.2 seaborn=0.11.0 -y
 
 # 3. install pytorch and torch_geometric
-conda install pytorch=1.9.0 cudatoolkit=10.2 -c pytorch -y
-conda install pyg=2.0.3 -c pyg -c conda-forge -y
+mamba install pytorch_geometric==2.0.3 -y
+mamba install pytorch_cluster -y
 
 # 4. install others
-conda install -c rdkit rdkit=2022.09.1 -y
-conda install -c conda-forge biopython=1.77 openbabel=3.1.1 -y
-conda install -c conda-forge plip=2.2.2
+mamba install -c rdkit rdkit=2022.09.1 -y
+mamba install -c conda-forge biopython=1.77 openbabel=3.1.1 -y
+mamba install -c conda-forge plip=2.2.2
+
+
